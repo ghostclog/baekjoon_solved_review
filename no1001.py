7e3d9f97724c1f967c8b2a314afd5a53366c2b5f
ghostdog <87572532+ghostclog@users.noreply.github.com>
2023-10-06 19:55:58 +0900

    Create no1001.py
    1001번 문제

---
+++ no1001.py
@@ -0,0 +1,17 @@
+#푼 시각: 2022년 5월 9일
+#난이도: 새싹
+#문제 내용
+'''
+두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오. / 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+'''
+
+flag = False
+while(flag == False):
+  a , b = map(int, input().split())
+  if(a > 0 and 10 >b):
+    flag = True
+print(a-b)
+
+'''
+1000번이랑 똑같은 방식으로 풀었다.
+'''
